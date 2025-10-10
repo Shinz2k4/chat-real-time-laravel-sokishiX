814bf4581e36ec6df734e89eea31e7a1382a9a85
Shinz2k4 <dminh2k4cn@gmail.com>
2025-10-10 22:36:02 +0700

    update pusher and optimize sending message

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,11 +36,13 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'host' => env('PUSHER_HOST', 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
-                'port' => env('PUSHER_PORT', 443),
-                'scheme' => env('PUSHER_SCHEME', 'https'),
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                // If PUSHER_HOST is empty string, fall back to null so SDK uses api-<cluster>.pusher.com
+                'host' => env('PUSHER_HOST') ?: null,
+                'port' => env('PUSHER_PORT') ?: 443,
+                'scheme' => env('PUSHER_SCHEME') ?: 'https',
                 'encrypted' => true,
-                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+                'useTLS' => (env('PUSHER_SCHEME') ?: 'https') === 'https',
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
--- resources/js/components/MessagesFeed.vue
+++ resources/js/components/MessagesFeed.vue
@@ -53,9 +53,11 @@ export default {
     },
     methods: {
         scrollToBottom() {
-            setTimeout(() => {
-                this.$refs.feed.scrollTop = this.$refs.feed.scrollHeight - this.$refs.feed.clientHeight;
-            }, 50);
+            this.$nextTick(() => {
+                if (this.$refs.feed) {
+                    this.$refs.feed.scrollTop = this.$refs.feed.scrollHeight;
+                }
+            });
         },
         formatTime(time) {
             return moment(time).format('h:mm a');
@@ -111,24 +113,36 @@ export default {
             } else if (minutes < 60) {
                 return `${minutes} minutes ago`;
             } else if (isToday) {
-                return getFormattedDate(date, 'Today'); // Today at 10:20
+                return this.getFormattedDate(date, 'Today'); // Today at 10:20
             } else if (isYesterday) {
-                return getFormattedDate(date, 'Yesterday'); // Yesterday at 10:20
+                return this.getFormattedDate(date, 'Yesterday'); // Yesterday at 10:20
             } else if (isThisYear) {
-                return getFormattedDate(date, false, true); // 10. January at 10:20
+                return this.getFormattedDate(date, false, true); // 10. January at 10:20
             }
 
-            return getFormattedDate(date); // 10. January 2017. at 10:20
+            return this.getFormattedDate(date); // 10. January 2017. at 10:20
         },
 
     },
     watch: {
-        contact: function (old) {
-            this.scrollToBottom();
+        contact: function (newContact, oldContact) {
+            if (newContact && newContact !== oldContact) {
+                this.scrollToBottom();
+            }
         },
-        messages: function (old) {
-            this.scrollToBottom();
+        messages: {
+            handler: function (newMessages, oldMessages) {
+                if (newMessages && newMessages.length > 0) {
+                    // Auto-scroll when new messages are added
+                    this.scrollToBottom();
+                }
+            },
+            deep: true
         },
+    },
+    mounted() {
+        // Scroll to bottom when component is mounted
+        this.scrollToBottom();
     },
 }
 </script>
